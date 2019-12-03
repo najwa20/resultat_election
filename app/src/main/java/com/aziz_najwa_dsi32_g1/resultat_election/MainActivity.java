18cde744050799a59d9062e9d588954cd866e163
aziz <azizba015@gmail.com>
2019-12-03 20:16:35 +0100

    Initial commit

---
+++ app/src/main/java/com/aziz_najwa_dsi32_g1/resultat_election/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aziz_najwa_dsi32_g1.resultat_election;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
