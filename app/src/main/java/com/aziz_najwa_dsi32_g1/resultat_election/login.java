e1f95ff72199c591ce2c3e6885a3f7d6d0b9fb54
aziz <azizba015@gmail.com>
2019-12-06 00:48:55 +0100

    affihage des election et de choix(exemple)
    ajouter les preferance partage au page

--- app/src/main/java/com/aziz_najwa_dsi32_g1/resultat_election/login.java
+++ app/src/main/java/com/aziz_najwa_dsi32_g1/resultat_election/login.java
@@ -1,9 +1,11 @@
 package com.aziz_najwa_dsi32_g1.resultat_election;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,6 +18,9 @@ public class login extends AppCompatActivity {
     Button btn;
     TextView txt;
     datahelper db;
+    CheckBox c;
+    SharedPreferences.Editor editor;
+    SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,12 @@ public class login extends AppCompatActivity {
         pwd = findViewById(R.id.user_password);
         btn = findViewById(R.id.id_cree);
         txt = findViewById(R.id.textView);
+        c = findViewById(R.id.checkBox2);
+        pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        editor = pref.edit();
+        login.setText(pref.getString("login", null));
+        pwd.setText(pref.getString("pwd", null));
+        c.setChecked(pref.getBoolean("c", false));
         txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,11 +49,22 @@ public class login extends AppCompatActivity {
             public void onClick(View view) {
                 String t1 = login.getText().toString();
                 String t2 = pwd.getText().toString();
+                if (c.isChecked()) {
+                    editor.putString("login", login.getText().toString());
+                    editor.putString("pwd", pwd.getText().toString());
+                    editor.putBoolean("c", true);
+                } else {
+                    editor.remove("login");
+                    editor.remove("pwd");
+                    editor.remove("c");
+                }
+                editor.commit();
                 if (db.chekpass(t1, t2)) {
                     if (t1 == "admin") {
                         Toast.makeText(getApplicationContext(), "succesfuly login admin", Toast.LENGTH_LONG).show();
                     } else {
-                        Toast.makeText(getApplicationContext(), "succesfuly login", Toast.LENGTH_LONG).show();
+                        Intent i = new Intent(getApplicationContext(), liste_election.class);
+                        startActivity(i);
                     }
                 } else {
                     Toast.makeText(getApplicationContext(), "woring email or password", Toast.LENGTH_LONG).show();
